9a7c4982f1d19762a9d0f3d7922c783fc26e3c99
Bradneal14 <bradleyhneal@gmail.com>
2016-03-15 18:28:01 -0700

    removes specs and adds readme

--- bin/basic_server.rb
+++ bin/basic_server.rb
@@ -9,6 +9,19 @@ app = Proc.new do |env|
   res.finish
 end
 
+router = Router.new
+router.draw do
+  get Regexp.new("^/cats$"), Cats2Controller, :index
+  get Regexp.new("^/cats/(?<cat_id>\\d+)/statuses$"), StatusesController, :index
+end
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  router.run(req, res)
+  res.finish
+end
+
 Rack::Server.start(
 app: app,
 Port: 3000
